1eb4a4635212a44faef09fbea7a706b5a7a2b9a0
Gaurav Anthwal <57260613+gauravanthwal@users.noreply.github.com>
2023-04-13 21:07:34 +0530

    Create Indicator-SMA.py

---
+++ Indicator-SMA.py
@@ -0,0 +1,10 @@
+//@version=5
+indicator(title = 'MA', overlay = true)
+
+sma20 = ta.sma(close, 20)
+sma50 = ta.sma(close, 50)
+sma200 = ta.sma(close, 200)
+
+plot(sma20, color = color.lime)
+plot(sma50, color = color.yellow)
+plot(sma200, color = color.purple)
